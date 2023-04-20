3b29cbf16755e37fb1ed0a1249d1e9f63ad76019
AdrianRi99 <adrian11820@gmail.com>
2023-04-20 17:44:30 +0200

    unit tests

---
+++ app/src/test/java/fh/uebung/tictactoe/CalculatorTest.kt
@@ -0,0 +1,19 @@
+package fh.uebung.tictactoe
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CalculatorTest {
+    @Test
+    fun testAddition() {
+        val calculator = Calculator()
+        val result = calculator.add(2, 3)
+        assertEquals(5, result)
+    }
+}
+
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
