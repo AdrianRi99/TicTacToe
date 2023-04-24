6ee1f709ff909c7441678a245f556fd35c80f477
AdrianRi99 <94113524+AdrianRi99@users.noreply.github.com>
2023-04-24 15:17:10 +0200

    Update log_input.js

--- .scripts/log_input.js
+++ .scripts/log_input.js
@@ -1,7 +1,41 @@
-const fs = require('fs');
+function showInputPrompt() {
+  const inputPrompt = document.getElementById('input-prompt');
+  inputPrompt.style.display = 'block';
+}
 
-const input = process.argv[2];
+function logInput() {
+  const inputText = document.getElementById('input-text').value;
+  const logFile = 'input-log.md';
+  const commitMessage = 'Added input to log file';
 
-const logFile = 'logs/mylog.txt';
+  const date = new Date();
+  const dateString = date.toLocaleString();
+  const logText = `## ${dateString}\n\n${inputText}\n\n`;
 
-fs.appendFileSync(logFile, input + '\n');
+  const path = 'logs/';
+  const fileName = `${date.toISOString().slice(0, 10)}-${logFile}`;
+  const filePath = `${path}${fileName}`;
+
+  const octokit = new Octokit();
+  octokit.rest.repos.createOrUpdateFileContents({
+    owner: 'AdrianRi99',
+    repo: 'TicTacToe.git',
+    path: filePath,
+    message: commitMessage,
+    content: btoa(logText),
+    committer: {
+      name: 'AdrianRi99',
+      email: 'adrian11820@gmail.com',
+    },
+    author: {
+      name: 'AdrianRi99',
+      email: 'adrian11820@gmail.com',
+    },
+  }).then(() => {
+    const successMessage = document.getElementById('success-message');
+    successMessage.style.display = 'block';
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
