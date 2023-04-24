34ec963d835e24da6333dc994014b5ed28353d49
AdrianRi99 <94113524+AdrianRi99@users.noreply.github.com>
2023-04-24 18:44:05 +0200

    Delete index.js

--- .my-custom-action/index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-
-try {
-  const message = core.getInput('message');
-  console.log(`The message is: ${message}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
